6982d5a3cf5015900c2133f339ee51a98280cfbb
JOCHYMSKI Hugo <hugo.jochymski@etu.unistra.fr>
2021-07-11 12:17:39 +0200

    Few functions fixes, cellPrint and clearCell added

--- gridO.h
+++ gridO.h
@@ -6,74 +6,99 @@
 */
 #include <curses.h>
 
-//Block apparence
-#define BLOCK_CORNER "+" /*!< Block corner apparence */
+//Cell apparence
+#define CELL_CORNER "+" /*!< Cell corner apparence */
 // (2y size recommended on default settings)
-#define BLOCK_SIZE_X 2 /*!< Block x-edge size */
-#define BLOCK_SIZE_Y 1 /*!< Block y-edge size */
-#define BLOCK_XAXE "-" /*!< Block x-axis edge apparence */
-#define BLOCK_YAXE "|" /*!< Block y-axis edge apparence */
+#define CELL_WIDTH 2 /*!< Cell x-edge size */
+#define CELL_HEIGHT 1 /*!< Cell y-edge size */
+#define CELL_SIZE (CELL_WIDTH*CELL_HEIGHT) /*!< Space within the cell */
+#define CELL_XAXE "-" /*!< Cell x-axis edge apparence */
+#define CELL_YAXE "|" /*!< Cell y-axis edge apparence */
+
+
+/******************************************************************************/
+/*********************************** UTILS ************************************/
+/******************************************************************************/
 
 /*!
    \brief Converts a y position on the grid to the real position on the file
    \param[in] y Y position to convert
-   \return The converted position (integer) or -1 on error
-   \note Possible errors : \p y < 0
+   \return The converted position (integer)
 */
 int yGridToReal(int y)
 {
-	if (y < 0) return -1;
-	else if (y == 0) return 0;
-	else return (y*BLOCK_SIZE_Y + y);
+	if (y == 0) return 0;
+	else return (y*CELL_HEIGHT + y);
 }
 
 /*!
    \brief Converts a x position on the grid to the real position on the file
    \param[in] x X position to convert
-   \return The converted position (integer) or -1 on error
-   \note Possible errors : \p x < 0
+   \return The converted position (integer)
 */
 int xGridToReal(int x)
 {
-	if (x < 0) return -1;
-	else if (x == 0) return 0;
-	else return (x*BLOCK_SIZE_X + x);
+	if (x == 0) return 0;
+	else return (x*CELL_WIDTH + x);
 }
 
 /*!
-   \brief Prints a block using the \p x and \p y grid coordinates
+   \brief Mimic the strlen function from string.h
+   \param[in] str String to be examinated
+   \return Length of \p str
+*/
+int strLen(char* str)
+{
+	int size = 0;
+	while (str[size])
+		size++;
+	return size;
+}
+
+/******************************************************************************/
+/********************************* BASIC GRID *********************************/
+/******************************************************************************/
+
+
+/*!
+   \brief Prints a cell using the \p x and \p y grid coordinates
    \param [in] x X coordinate
    \param [in] y Y coordinate
+   \return -1 on error, 0 on succes
 */
-void gp_block(int x, int y)
+int gp_cell(int x, int y)
 {
+	if (x < 0 || y < 0)
+		return -1;
+
 	//Corners
-	mvprintw(yGridToReal(y), xGridToReal(x), BLOCK_CORNER);
-	mvprintw(yGridToReal(y), xGridToReal(x)+BLOCK_SIZE_X+1, BLOCK_CORNER);
-	mvprintw(yGridToReal(y)+BLOCK_SIZE_Y+1, xGridToReal(x), BLOCK_CORNER);
-	mvprintw(yGridToReal(y)+BLOCK_SIZE_Y+1,
-		 xGridToReal(x)+BLOCK_SIZE_X+1, BLOCK_CORNER);
+	mvprintw(yGridToReal(y), xGridToReal(x), CELL_CORNER);
+	mvprintw(yGridToReal(y), xGridToReal(x)+CELL_WIDTH+1, CELL_CORNER);
+	mvprintw(yGridToReal(y)+CELL_HEIGHT+1, xGridToReal(x), CELL_CORNER);
+	mvprintw(yGridToReal(y)+CELL_HEIGHT+1,
+		 xGridToReal(x)+CELL_WIDTH+1, CELL_CORNER);
 
 	//x-axis edges
-	for (int i=0; i<BLOCK_SIZE_X; i++)
+	for (int i=0; i<CELL_WIDTH; i++)
 	{
 		//top
-		mvprintw(yGridToReal(y), xGridToReal(x)+i+1, BLOCK_XAXE);
+		mvprintw(yGridToReal(y), xGridToReal(x)+i+1, CELL_XAXE);
 		//bot
-		mvprintw(yGridToReal(y)+BLOCK_SIZE_Y+1,
-			 xGridToReal(x)+i+1, BLOCK_XAXE);
+		mvprintw(yGridToReal(y)+CELL_HEIGHT+1,
+			 xGridToReal(x)+i+1, CELL_XAXE);
 	}
 
 	//y-axis edges
-	for (int i=0; i<BLOCK_SIZE_Y; i++)
+	for (int i=0; i<CELL_HEIGHT; i++)
 	{
 		//left
-		mvprintw(yGridToReal(y)+i+1, xGridToReal(x), BLOCK_YAXE);
+		mvprintw(yGridToReal(y)+i+1, xGridToReal(x), CELL_YAXE);
 		//right
 		mvprintw(yGridToReal(y)+i+1,
-			 xGridToReal(x)+BLOCK_SIZE_X+1, BLOCK_YAXE);
+			 xGridToReal(x)+CELL_WIDTH+1, CELL_YAXE);
 
 	}
+	return 0;
 }
 
 /*!
@@ -81,23 +106,90 @@ void gp_block(int x, int y)
    \p start on the x-axis
    \param[in] start Starting point on the x-axis
    \param[in] y Y coordinate
-   \param[in] length Line length
+   \param[in] len Line length
+   \return -1 on error, 0 on succes
 */
-void gp_line(int start, int y, int length)
+int gp_line(int start, int y, int len)
 {
-	for (int i=start; i<length; i++)
-		gp_block(i, y);
+	if (start < 0 || y < 0 || len < 0)
+		return -1;
+
+	for (int i=start; i<len; i++)
+		gp_cell(i, y);
+	return 0;
 }
 
 /*!
-   \brief Prints a \p length long row at the \p x coordinate starting at \
+   \brief Prints a \p len long row at the \p x coordinate starting at \
    \p start on the y-axis
    \param[in] x X coordinate
    \param[in] start Starting point on the y-axis
-   \param[in] length Row length
+   \param[in] len Row length
+   \return -1 on error, 0 on succes
 */
-void gp_row(int x, int start, int length)
+int gp_row(int x, int start, int len)
 {
-	for (int i=start; i<length; i++)
-		gp_block(x, i);
+	if (start < 0 || x < 0 || len < 0)
+		return -1;
+
+	for (int i=start; i<len; i++)
+		gp_cell(x, i);
+	return 0;
+}
+
+
+/******************************************************************************/
+/****************************** CELL MANAGEMENT *******************************/
+/******************************************************************************/
+
+/*!
+   \brief Prints \p content to a (\p x , \p y ) cell
+   \param[in] x X coordinate
+   \param[in] y Y coordinate
+   \param[in] content What to be print
+   \return -1 on error, 0 on succes
+   \note \p content length has to be smaller or egal to CELL_SIZE \
+   otherwhise it will be truncated
+*/
+int gp_cellPrint(int x, int y, char* content)
+{
+	if (x < 0 || y < 0 || content == NULL)
+		return -1;
+
+	int len = strLen(content);
+	int istr = 0;
+
+	for (int iy=yGridToReal(y)+1; iy<CELL_HEIGHT+yGridToReal(y)+1; iy++)
+	{
+		for (int ix=xGridToReal(x)+1; ix<CELL_WIDTH+xGridToReal(x)+1; ix++)
+		{
+			if (istr < len)
+				mvprintw(iy, ix, "%c", content[istr++]);
+		}
+	}
+
+	return 0;
+}
+
+/*!
+   \brief Erases cell content
+   \param[in] x X coordinate
+   \param[in] y Y coordinate
+   \return -1 on error, 0 on succes
+*/
+int gp_clearCell(int x, int y)
+{
+	if (x < 0 || y < 0)
+		return -1;
+
+	//Through x-axis
+	for (int iy=yGridToReal(y)+1; iy<CELL_HEIGHT+yGridToReal(y)+1; iy++)
+	{
+		//Through y-axis
+		for (int ix=xGridToReal(x)+1; ix<CELL_WIDTH+xGridToReal(x)+1; ix++)
+		{
+			mvprintw(iy, ix, " ");
+		}
+	}
+	return 0;
 }
