56fec0f38689090b29157be40ce13372538eef9b
JOCHYMSKI Hugo <hugo.jochymski@etu.unistra.fr>
2021-07-12 20:19:55 +0200

    Colors for row and line added

--- gridO.h
+++ gridO.h
@@ -72,11 +72,10 @@ int strLen(char* str)
 /********************************* BASIC GRID *********************************/
 /******************************************************************************/
 
-
 /*!
    \brief Prints a cell using the \p x and \p y grid coordinates
-   \param [in] x X coordinate
-   \param [in] y Y coordinate
+   \param[in] x X coordinate
+   \param[in] y Y coordinate
    \return -1 on error, 0 on succes
 */
 int gp_cell(int x, int y)
@@ -152,6 +151,111 @@ int gp_row(int x, int start, int len)
 
 
 /******************************************************************************/
+/******************************** COLORED GRID ********************************/
+/******************************************************************************/
+
+/*!
+   \brief Prints a \color cell using the \p x and \p y grid coordinates
+   \param[in] x X coordinate
+   \param[in] y Y coordinate
+   \param[in] color The color to be used
+   \return -1 on error, 0 on succes
+   \note Color is enabled at the beginning and disabled at the end
+*/
+int gpc_cell(int x, int y, int color)
+{
+	if (x < 0 || y < 0)
+		return -1;
+
+	//Turn color on
+	if (attron(COLOR_PAIR(color)) == ERR) return -1;
+
+	//Corners
+	mvprintw(yGridToReal(y), xGridToReal(x), CELL_CORNER);
+	mvprintw(yGridToReal(y), xGridToReal(x)+CELL_WIDTH+1, CELL_CORNER);
+	mvprintw(yGridToReal(y)+CELL_HEIGHT+1, xGridToReal(x), CELL_CORNER);
+	mvprintw(yGridToReal(y)+CELL_HEIGHT+1,
+		 xGridToReal(x)+CELL_WIDTH+1, CELL_CORNER);
+
+	//x-axis edges
+	for (int i=0; i<CELL_WIDTH; i++)
+	{
+		//top
+		mvprintw(yGridToReal(y), xGridToReal(x)+i+1, CELL_XAXE);
+		//bot
+		mvprintw(yGridToReal(y)+CELL_HEIGHT+1,
+			 xGridToReal(x)+i+1, CELL_XAXE);
+	}
+
+	//y-axis edges
+	for (int i=0; i<CELL_HEIGHT; i++)
+	{
+		//left
+		mvprintw(yGridToReal(y)+i+1, xGridToReal(x), CELL_YAXE);
+		//right
+		mvprintw(yGridToReal(y)+i+1,
+			 xGridToReal(x)+CELL_WIDTH+1, CELL_YAXE);
+
+	}
+
+	//Turn color off
+	if (attroff(COLOR_PAIR(color)) == ERR) return -1;
+	return 0;
+}
+
+/*!
+   \brief Prints a \p length \p color long line at the \p y coordinate \
+   starting at \p start on the x-axis
+   \param[in] start Starting point on the x-axis
+   \param[in] y Y coordinate
+   \param[in] len Line length
+   \param[in] color The color to be used
+   \return -1 on error, 0 on succes
+*/
+int gpc_line(int start, int y, int len, int color)
+{
+	if (start < 0 || y < 0 || len < 0)
+		return -1;
+
+	//Turn color on
+	if (attron(COLOR_PAIR(color)) == ERR) return -1;
+
+	for (int i=start; i<len; i++)
+		if (gp_cell(i, y) == -1) return -1;
+
+	//Turn color off
+	if (attroff(COLOR_PAIR(color)) == ERR) return -1;
+
+	return 0;
+}
+
+/*!
+   \brief Prints a \p len long \p color row at the \p x coordinate starting at \
+   \p start on the y-axis
+   \param[in] x X coordinate
+   \param[in] start Starting point on the y-axis
+   \param[in] len Row length
+   \param[in] color The color to be used
+   \return -1 on error, 0 on succes
+*/
+int gpc_row(int x, int start, int len, int color)
+{
+	if (start < 0 || x < 0 || len < 0)
+		return -1;
+
+	//Turn color on
+	if (attron(COLOR_PAIR(color)) == ERR) return -1;
+
+	for (int i=start; i<len; i++)
+		if (gp_cell(x, i) == -1) return -1;
+
+	//Turn color off
+	if (attroff(COLOR_PAIR(color)) == ERR) return -1;
+
+	return 0;
+}
+
+/******************************************************************************/
 /****************************** CELL MANAGEMENT *******************************/
 /******************************************************************************/
 
@@ -216,19 +320,21 @@ int gp_clearCell(int x, int y)
 
 /*!
    \brief Init color pairs to be used later
-   \return "Return of the function"
+   \return -1 on error, 0 on succes
 */
-void init_colorpairs()
+int init_colors()
 {
-	init_pair(CLR_YLW, COLOR_YELLOW, COLOR_BLACK);
-	init_pair(CLR_BLU, COLOR_BLUE, COLOR_BLACK);
-	init_pair(CLR_RED, COLOR_RED, COLOR_BLACK);
-	init_pair(CLR_GRN, COLOR_GREEN, COLOR_BLACK);
-	init_pair(CLR_WHT, COLOR_WHITE, COLOR_BLACK);
-	init_color(COLOR_ORANGE, 1000, 500, 0);
-	init_pair(CLR_ORG, COLOR_ORANGE, COLOR_BLACK);
-	init_pair(CLR_VLT, COLOR_MAGENTA, COLOR_BLACK);
-	init_color(COLOR_PINK, 1000, 411, 705);
-	init_pair(CLR_PNK, COLOR_PINK, COLOR_BLACK);
-	init_pair(CLR_CYN, COLOR_CYAN, COLOR_BLACK);
+	if (start_color() == ERR) return -1;
+	if (init_pair(CLR_YLW, COLOR_YELLOW, COLOR_BLACK)  == ERR) return -1;
+	if (init_pair(CLR_BLU, COLOR_BLUE, COLOR_BLACK)    == ERR) return -1;
+	if (init_pair(CLR_RED, COLOR_RED, COLOR_BLACK) 	   == ERR) return -1;
+	if (init_pair(CLR_GRN, COLOR_GREEN, COLOR_BLACK)   == ERR) return -1;
+	if (init_pair(CLR_WHT, COLOR_WHITE, COLOR_BLACK)   == ERR) return -1;
+	if (init_color(COLOR_ORANGE, 1000, 500, 0) 	   == ERR) return -1;
+	if (init_pair(CLR_ORG, COLOR_ORANGE, COLOR_BLACK)  == ERR) return -1;
+	if (init_pair(CLR_VLT, COLOR_MAGENTA, COLOR_BLACK) == ERR) return -1;
+	if (init_color(COLOR_PINK, 1000, 411, 705) 	   == ERR) return -1;
+	if (init_pair(CLR_PNK, COLOR_PINK, COLOR_BLACK)    == ERR) return -1;
+	if (init_pair(CLR_CYN, COLOR_CYAN, COLOR_BLACK)    == ERR) return -1;
+	return 0;
 }
